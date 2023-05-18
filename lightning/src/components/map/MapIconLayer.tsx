ad38b676d64f442685d2bd543b6f13d72be4b150
Adam <24621027+adoyle0@users.noreply.github.com>
2023-05-17 21:22:10 -0400

    testing logs

--- lightning/src/components/map/MapIconLayer.tsx
+++ lightning/src/components/map/MapIconLayer.tsx
@@ -15,6 +15,11 @@ export default function MapIconLayer(props: any) {
 
     createEffect(() => console.log("Watch me update but not trigger a render!", stations()));
 
+    function deezCompare() {
+        console.log('comparing');
+        return false
+    }
+    
     return (
         <Show when={stations()}>
             <Layer customLayer={
@@ -30,6 +35,8 @@ export default function MapIconLayer(props: any) {
                     getPosition: d => [d.Loc.Coordinates[1], d.Loc.Coordinates[0]],
                     getSize: d => 3,
                     getColor: d => [d.Dist * 100, 140 - (d.Dist * 50), 0],
+                    dataComparator: deezCompare,
+                    _dataDiff: () => console.log('datadiff'),
                     parameters: {
                         depthTest: false,
                     },
